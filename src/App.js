a2061eec121392c3b2c27075c4a5f6ec41683a75
runandrerun <the.asantiagojr@gmail.com>
2018-11-27 18:39:54 -0500

    React and Redux barebones setup is completed.

--- src/App.js
+++ src/App.js
@@ -2,30 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const theme = createMuiTheme({
-  typography: {
-    fontFamily: "'Poppins', sans-serif",
-    fontSize:14,
-    textTransform: "none",
-    color: "#484848",
-  },
-  palette: {
-    primary: {
-      light: "#ff8e8c",
-      main: "#484848",
-      dark: "#c62035",
-      contrastText: "#fff"
-    },
-    secondary: {
-      light: "#4da9b7",
-      main: "#ff5a5f",
-      dark: "#004e5a",
-      contrastText: "#000"
-    }
-  },
-});
-
 class App extends Component {
+  // 
+  // const theme = createMuiTheme({
+  //   typography: {
+  //     fontFamily: "'Poppins', sans-serif",
+  //     fontSize:14,
+  //     textTransform: "none",
+  //     color: "#484848",
+  //   },
+  //   palette: {
+  //     primary: {
+  //       light: "#ff8e8c",
+  //       main: "#484848",
+  //       dark: "#c62035",
+  //       contrastText: "#fff"
+  //     },
+  //     secondary: {
+  //       light: "#4da9b7",
+  //       main: "#ff5a5f",
+  //       dark: "#004e5a",
+  //       contrastText: "#000"
+  //     }
+  //   },
+  // });
+
   render() {
     return (
       <div className="App">
