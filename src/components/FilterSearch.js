6186fa9d9a79dcfc0f3c67332ba1cf01e93c771f
runandrerun <the.asantiagojr@gmail.com>
2018-11-30 08:52:58 -0500

    Material-UI imports

--- src/components/FilterSearch.js
+++ src/components/FilterSearch.js
@@ -1,5 +1,15 @@
-// FilterBar.js
-// imports excluded for brevity
+import React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import { withStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import Grid from "@material-ui/core/Grid";
+import Explore from "@material-ui/icons/Explore";
+import Button from "@material-ui/core/Button";
+
 const styles = theme => ({
   header: {
     height: "65px",
